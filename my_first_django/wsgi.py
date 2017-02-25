777c4c35fd5034a677ae64b050cb375dd97c8690
Tamara Temple <tamouse@gmail.com>
2017-02-25 11:06:46 -0600

    Blog urls and views added for post listing

--- my_first_django/wsgi.py
+++ my_first_django/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+app_path = os.path.dirname(__file__)
+if app_path not in sys.path:
+    sys.path.append(app_path)
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_first_django.settings")
 
-application = get_wsgi_application()
+application = StaticFilesHandler(get_wsgi_application())
