8827e3a1e6065012a5c3438ed8bf4913fff914d5
Tamara Temple <tamouse@gmail.com>
2017-03-18 11:25:15 -0500

    Mostly update the README with the changes requires to make the app work
    - document how to set up the app on a clean system
    - setting.py needs some specific values set when you copy it from settings.py.sample
    - clean up comments and other minor PEP-8 complaints
    - create requirements.txt to save pip library configuration

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-My first Django App
+# My first Django App
+
+
+Following the Django Girls Tutorial at https://tutorial.djangogirls.org/en/
+
+If rebuilding, need to:
+
+- make a virtual env for python 3.x
+- upggrade pip
+- pip install from requirements.txt
+- copy settings.py.sample to settings.py
+
+## Settings.PY
+
+- create the secret key (make it up)
+- add 'blog' to the installed apps
--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
 
-    ## NOTE-TO-SELF:
-    ## This is setting the default to the function,
-    ## NOT the value, otherwise every object would
-    ## have the time the class was read in.
+    # NOTE-TO-SELF:
+    # This is setting the default to the function,
+    # NOT the value, otherwise every object would
+    # have the time the class was read in.
     created_at = models.DateTimeField(
         default=timezone.now,
         blank=False,
@@ -28,11 +29,11 @@ class Post(models.Model):
     )
 
     def publish(self):
-        ## NOTE-TO-SELF:
-        ## This time, we want the exact timestamp, so
-        ## we're calling the now method
+        # NOTE-TO-SELF:
+        # This time, we want the exact timestamp, so
+        # we're calling the now method
         self.published_at = timezone.now()
-        self.save
+        self.save()
 
     def __str__(self):
         return self.title
